dea4398abdbe74155349d222a88e108ebae201b3
Hwang Seok Ju <asa4163@gmail.com>
2022-11-17 22:12:04 +0900

    ServerMain change
    InPacket 추가
    SocketWithIndex추가
    ClientMgr 함수 수정
    main에서 Thread 생성

--- GameServer/ServerMain.cpp
+++ GameServer/ServerMain.cpp
@@ -19,6 +19,12 @@ enum  CIRCLE_STATE {
 	CIRCLE_OFF
 };
 
+struct InPacket {
+	float x_angle, y_angle;
+	glm::vec3 arrowPosition;
+	glm::vec3 arrowRotation;
+};
+
 struct Packet {
 	float x_angle, y_angle; // 플레이어의 시야 각도 값
 	glm::vec3 arrowPosition; // 화살의 좌표 값;
@@ -33,6 +39,12 @@ struct InitPacket {
 	glm::vec3 circleCenter[CIRCLENUM]; // 과녁의 중앙의 위치 값
 	glm::vec3 player1Pos; // 플레이어 1의 위치
 	glm::vec3 player2Pos; // 플레이어 2의 위치
+};
+
+// 소켓과 함께 인덱스를 넘겨줌
+struct SocketWithIndex {
+	SOCKET sock;
+	int index;
 };
 
 short g_circleState[CIRCLENUM];
@@ -52,20 +64,71 @@ int client_index = 0;
 HANDLE hReadEvent[2];
 HANDLE hWriteEvent[2];
 
-DWORD WINAPI ClientMgr() {
+InPacket g_InPacket[2];
+Packet g_Packet[2];
+
+DWORD WINAPI ClientMgr(LPVOID arg) { // arg로 SocketWithIndex가 넘어옴
+	SocketWithIndex* swi = (SocketWithIndex*)arg;
+	SOCKET client_sock = swi->sock;
+	int index = swi->index;
+
+	int opositeIndex;
+	if (index == 1)
+		opositeIndex = 0;
+	else
+		opositeIndex = 1;
+
 	DWORD retval;
-	if (client_index < 2) {
-		// 아무도없거나 한명일때 처리하는 부분 넣어야됨.
+
+	// 초기화값 전송
+	// 초기화값을 어디서 보내는게 좋을까용
+	retval = send(client_sock, (char*)&g_InitPacket, sizeof(InitPacket), 0);
+	if (retval == SOCKET_ERROR) {
+		err_display("send()");
+		return 1;
 	}
-	while (client_index == 2) { // 두명일때
-		for (int i = 0; i < 2; ++i) {
-			retval = WaitForSingleObject(hReadEvent[i], INFINITE);
-			if (retval != WAIT_OBJECT_0)
-				break;
+
+	while (1) {
+		// 클라이언트로부터 데이터 수신
+		// 이벤트는 추후에 추가
+		retval = recv(client_sock, (char*)&g_InPacket[index], sizeof(InPacket), MSG_WAITALL);
+		if (retval == SOCKET_ERROR) {
+			err_display("recv()");
+			break;
+		}
+		else if (retval == 0)
+			break;
+
+		// 현재 인덱스의 클라이언트 정보를 상대 클라이언트에게 넘겨줄 패킷에 저장
+		g_Packet[opositeIndex].arrowPosition = g_InPacket[index].arrowPosition;
+		g_Packet[opositeIndex].arrowRotation = g_InPacket[index].arrowRotation;
+		g_Packet[opositeIndex].x_angle = g_InPacket[index].x_angle;
+		g_Packet[opositeIndex].y_angle=	g_InPacket[index].y_angle;
+
+		// 클라이언트로 데이터 송신
+		// 이벤트는 추후에 추가
+		retval = send(client_sock, (char*)&g_Packet[index], sizeof(Packet), 0);
+		if (retval == SOCKET_ERROR) {
+			err_display("send()");
+			break;
 		}
 	}
 
+	closesocket(client_sock);
 	return 0;
+
+	//if (client_index < 2) {
+	//	// 아무도없거나 한명일때 처리하는 부분 넣어야됨.
+	//}
+	//while (client_index == 2) { // 두명일때
+	//	for (int i = 0; i < 2; ++i) {
+	//		retval = WaitForSingleObject(hReadEvent[i], INFINITE);
+	//		if (retval != WAIT_OBJECT_0)
+	//			break;
+	//	}
+	//}
+
+	//return 0;
 }
 
 
@@ -116,7 +179,6 @@ int main(int argc, char* argv[])
 	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (listen_sock == INVALID_SOCKET) err_quit("socket()");
 
-	
 	// bind()
 	struct sockaddr_in serveraddr;
 	memset(&serveraddr, 0, sizeof(serveraddr));
@@ -131,34 +193,51 @@ int main(int argc, char* argv[])
 	if (retval == SOCKET_ERROR) err_quit("listen()");
 
 	// 데이터 통신에 사용할 변수
-	SOCKET client_sock;
-	struct sockaddr_in clientaddr;
-	int addrlen;
+	SOCKET client_sock[2];
+	struct sockaddr_in clientaddr[2];
+	int addrlen[2];
+	SocketWithIndex swi[2];
+	HANDLE hThread;
 
 	while (1) {
 		// accept()
-		addrlen = sizeof(clientaddr);
-		client_sock = accept(listen_sock, (struct sockaddr*)&clientaddr, &addrlen);
-		if (client_sock == INVALID_SOCKET) {
+		addrlen[0] = sizeof(clientaddr[0]);
+		client_sock[0] = accept(listen_sock, (struct sockaddr*)&clientaddr[0], &addrlen[0]);
+		if (client_sock[0] == INVALID_SOCKET) {
 			err_display("accept()");
 			break;
 		}
+		swi[0].sock = client_sock[0];
+		swi[0].index = 0;
+
+		// 2번째 클라이언트도 받게 대기
+
+		addrlen[1] = sizeof(clientaddr[1]);
+		client_sock[1] = accept(listen_sock, (struct sockaddr*)&clientaddr[1], &addrlen[1]);
+		if (client_sock[1] == INVALID_SOCKET) {
+			err_display("accept()");
+			break;
+		}
+		swi[1].sock = client_sock[1];
+		swi[1].index = 0;
+
+		// 통신을 위한 ClinetMgr 시작
+		hThread = CreateThread(NULL, 0, ClientMgr, &swi[0], 0, NULL);
+		if (hThread == NULL) closesocket(client_sock[0]);
+		else CloseHandle(hThread);
+		hThread = CreateThread(NULL, 0, ClientMgr, &swi[1], 0, NULL);
+		if (hThread == NULL) closesocket(client_sock[1]);
+		else CloseHandle(hThread);
 
 		// 접속한 클라이언트 정보 출력
-		char addr[INET_ADDRSTRLEN];
-		inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
-		printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
-			addr, ntohs(clientaddr.sin_port));
-
-		// 클라이언트와 데이터 통신
-		while (1) {
-			// 데이터
-		}
+		//char addr[INET_ADDRSTRLEN];
+		//inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+		//printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n", addr, ntohs(clientaddr.sin_port));
 
 		// 소켓 닫기
-		closesocket(client_sock);
-		printf("[TCP 서버] 클라이언트 종료: IP 주소=%s, 포트 번호=%d\n",
-			addr, ntohs(clientaddr.sin_port));
+		closesocket(client_sock[0]);
+		closesocket(client_sock[1]);
+		//printf("[TCP 서버] 클라이언트 종료: IP 주소=%s, 포트 번호=%d\n", addr, ntohs(clientaddr.sin_port));
 	}
 
 	// 소켓 닫기
